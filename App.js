0ced90261fb9180ab3e278979fa20a2c7a0ab60c
Bryan Machado <bryan.machado.soares@gmail.com>
2024-02-16 14:25:04 -0300

    tentando fazer modelo simples

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <View style={styles.profile}>
+          
+          <Text style={{color: '#fff'}}>Cleitin Perulebes</Text>
+        </View>
+      </View>
+      <View style={styles.content}>
+        <Image style={styles.profileImage} source={require('./assets/icon.png')} />
+      </View>
+      <View style={styles.footer}>
+        <Text style={{color: '#fff'}}>Rodapé</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +25,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'bottom',
+    justifyContent: 'center',
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#0000ff',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 3,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    flex: 1,
+    backgroundColor: '#0000ff',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profile: {
+    flexDirection: 'row',
+    columnGap: 10,
+    alignItems: 'center'
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderTopLeftRadius: 50,
+  borderTopRightRadius: 50,
+  borderBottomLeftRadius: 50,
+  borderBottomRightRadius: 50
+  }
 });
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Comandos uteis
+
+ ### criar um novo projeto react native
+
+```
+npx create-expo app --template
+```
+
+### testar no celular com tunnel
+
+1) Baixar o app Expo Go no celular
+2) Instalar o tunnel
+```
+expo start --tunnel
+```

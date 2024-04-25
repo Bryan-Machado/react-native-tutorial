fee7be4f9984236c78bd51ffff59a222ea1efbfa
Bryan-Machado <bryan.machado.soares@gmail.com>
2024-04-25 15:36:52 -0300

    comecando a usar async storage

--- screens/Products.js
+++ screens/Products.js
@@ -1,16 +1,45 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View, StyleSheet, TextInput } from "react-native"
 import Button from "../components/ui/Button"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { useNavigation } from "@react-navigation/native"
-
-const navigation = useNavigation()
+import { useEffect, useState } from "react"
 
 const Products = () => {
+
+    const [nameUser, setNameUser] = useState('')
+
+    const saveAs = async (data, value) => {
+        await AsyncStorage.setItem(data, value)
+    }
+    const getAs = async (data) => {
+        let dataFound = null
+        try {
+            dataFound = await AsyncStorage.getItem(data)
+            return dataFound
+        } catch (error) {
+         console.log(error);   
+        }
+    }
+
+    useEffect(() => {
+        const getNome = async () => {
+            const nome = await getAs('nome')
+            setNameUser(nome)
+        }
+        setNameUser()
+    }, [])
+
     return (
         <View style={styles.container}>
-            <Text>Products</Text>
-            <Button title="Cadastrar User" 
-            onPress={() => navigation.navigate('Cadastrar')}/>
+            <Text>nome salvo: {nameUser}</Text>
+            <TextInput 
+                placeholder="Nome"
+                onChangeText={setNameUser}
+                value={nameUser}
+            />
+            <Text>exemplo AsyncStorage</Text>
+            <Button title="Cadastrar Nome User" 
+            onPress={() => saveAs('nome', 'Cleitin Perulebes')}/>
         </View>
     )
 }
